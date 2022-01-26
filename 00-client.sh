4943b6ed8f8dcc14230b15329ac499a2de69d27d
Christophe Fontaine <cfontain@redhat.com>
2022-01-26 14:01:23 +0100

    Update testtols

--- 00-client.sh
+++ 00-client.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 rm -f /tmp/testpmd-net1
 rm -f /tmp/testpmd-net2
-gdb --args ./build/app/dpdk-testpmd -m 1024 -l 1,3 \
+gdb --args ./build/app/dpdk-testpmd -m 1024 -l 2,7,8 \
 	--file-prefix=testpmd_client_ \
-	--vdev=net_virtio_user0,path=/tmp/testpmd-net1,server=1,queues=1,packed_vq=1 \
-	--vdev=net_virtio_user1,path=/tmp/testpmd-net2,server=1,queues=1,packed_vq=1 \
-	--no-pci --proc-type=auto -- --no-lsc-interrupt -i
+	--vdev=net_virtio_user0,path=/tmp/testpmd-net1,server=1,queues=2 \
+	--vdev=net_virtio_user1,path=/tmp/testpmd-net2,server=1,queues=2 \
+	--no-pci --proc-type=auto \
+	--log-level='pmd.net.virtio.*',8 \
+	-- --no-lsc-interrupt -i --rxq=2 --txq=2 --nb-cores=2
 reset
+# --rxq=2 --txq=2
--- 01-switch-start.sh
+++ 01-switch-start.sh
@@ -13,6 +13,20 @@ show config fwd
 start
 EOF
 
-gdb --args ./build/app/dpdk-testpmd -m 1024 -l 1,2 -a 0000:00:00.0 --file-prefix=testpmd_vswitch_ \
-	-- --no-lsc-interrupt --forward-mode=flow -i  --cmdline-file=script
+#gdb --args ./build/app/dpdk-testpmd -m 1024 -l 1,2 -a 0000:00:00.0 --file-prefix=testpmd_vswitch_ \
+#	-- --no-lsc-interrupt --forward-mode=flow -i  --cmdline-file=script
+
+
+gdb --args ./build/app/dpdk-testpmd -m 1024 -l 2,3,4 -a 0000:05:00.1 \
+	--vdev="net_vhost2,iface=/tmp/vhost2.sock,client=1,queues=2" \
+	--vdev="net_vhost0,iface=/tmp/testpmd-net1,client=1,queues=2" \
+	--vdev="net_vhost1,iface=/tmp/testpmd-net2,client=1,queues=2" \
+	--file-prefix=testpmd_vswitch_ \
+	--log-level='pmd.net.vhost.*',8 \
+	--log-level='lib.vhost.*',8 \
+	-- --no-lsc-interrupt --forward-mode=flow -i  --nb-cores=2 --rxq=2 --txq=2
 reset
+exit 0
+	--auto-start
+	--vdev="net_vhost0,iface=/tmp/testpmd-net1,client=1,queues=2" \
+	--vdev="net_vhost1,iface=/tmp/testpmd-net2,client=1,queues=2" \
